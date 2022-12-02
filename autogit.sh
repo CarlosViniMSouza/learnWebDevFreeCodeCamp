fbc510916888264cc5bef373761de8e4d59d7d54
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-02 16:11:34 -0300

    Next Step: html comments

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,4 @@
-git init
 git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/learnWebDevFreeCodeCamp.git
+git commit -m "Next Step: html comments"
 git push -u origin main
 git status
